a6abf52234ab008f30466032f153c32b8f22b4ab
Ryansdee <desch.ryann@gmail.com>
2024-02-01 16:08:36 +0100

    new file:   contact.css 	new file:   contact.html 	modified:   css/dessert.css 	new file:   process-contact-form.php

---
+++ process-contact-form.php
@@ -0,0 +1,17 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    $to = "contact@eatwithloor.com";
+    $subject = "New Contact Form Submission";
+    $headers = "From: $email";
+
+    mail($to, $subject, $message, $headers);
+
+    // Vous pouvez rediriger l'utilisateur vers une page de confirmation
+    header("Location: thank-you.html");
+    exit();
+}
+?>
