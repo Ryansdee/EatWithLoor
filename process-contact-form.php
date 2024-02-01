324203ab892c9962e8efb2354266d13f67c3c1d4
Ryansdee <desch.ryann@gmail.com>
2024-02-01 18:30:12 +0100

    modified:   contact.css 	modified:   contact.html 	modified:   process-contact-form.php

--- process-contact-form.php
+++ process-contact-form.php
@@ -4,7 +4,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
     $message = $_POST["message"];
 
-    $to = "contact@eatwithloor.com";
+    $to = "ryan.deschuyteneer@gmail.com";
     $subject = "New Contact Form Submission";
     $headers = "From: $email";
 
